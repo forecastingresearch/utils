768e786db984cef2b3ac7bb4f7400458969465ff
Houtan Bastani <houtan@forecastingresearch.org>
2024-03-25 13:48:43 -0400

    chore: allow for various import statements

---
+++ __init__.py
@@ -0,0 +1,6 @@
+"""Utilities package for various helper functions and classes."""
+
+__all__ = [
+    "gcp",
+]
+from . import gcp
---
+++ gcp/__init__.py
@@ -0,0 +1,3 @@
+"""Google Cloud Platform related utilities."""
+
+from . import storage
